80b7a4e7199ad031760aa4a7963bcf55b61de2e9
Muhammad Nasir <muhammadnasiribrahim@gmail.com>
2025-01-10 23:52:47 +0100

    Your commit message here

--- app/frontend/main_ui.py
+++ app/frontend/main_ui.py
@@ -1,18 +1,53 @@
 import flet as ft
 
+COLOR =ft.LinearGradient(
+                begin=ft.alignment.top_left,
+                end=ft.alignment.bottom_right,
+                colors=["green","blue","green",],
+            ) 
 
-def main(page: ft.page):
-    page.window.width = 380
-    page.window.heiht = 480
+def main_ui():
 
-    _maim = ft.Column(
+    wel=ft.Column(
         controls=[
+            ft.Text(value="Welcome",size=25,weight="bold"),
+            ft.Text(value=".       to",size=20,weight="bold"),
+            ft.Text(value="Debt Tracker",size=20,weight="bold")
+        ]
+    )
+    return ft.Column(
+        controls=[
+            wel,
+            ft.Row([
+                ft.Container(
+                    height=160,gradient=COLOR,
+                    border_radius=7,width=170
+                ),
+                ft.Column([
+                    ft.Container(
+                    height=75,gradient=COLOR,
+                    border_radius=7,width=170,
+                    padding=ft.padding.only(bottom=0),
+                ),
+                ft.Container(
+                    height=75,gradient=COLOR,
+                    border_radius=7,width=170,
+                    padding=ft.padding.only(bottom=0)
+
+                )
+                ])
+            ]),
+            ft.Container(
+                height=80,gradient=COLOR,
+                border_radius=10,
+            ),
+            ft.Text(value="Recent debt",size=15,weight="bold",),
+            
+            ft.Container(
+                height=80,gradient=COLOR,
+                border_radius=10,
+            ),
+            ft.Text(value="Recent debt",size=15,weight="bold",),
             
         ]
     )
-
-    page.add()
-    page.update()
-
-if __name__=="__main__":
-    ft.app(target=main)    
